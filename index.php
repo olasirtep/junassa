4c75bb198a38fa0fb7785d04bf6dd2a6b3c7040e
Petri Salo <petri.salo@student.hamk.fi>
2018-12-03 15:11:10 +0000

    Fixed PHP syntax

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $query = (isset($_GET['q'])) ? $_GET['q'] : false;
+    $id = (isset($_GET['id'])) ? $_GET['id'] : false;
+    $error = (isset($_GET['e'])) ? $_GET['e'] : false;
+    include 'templates/main.html';
+    
+    if ($error) {
+        echo '<script>';
+        echo '$(showSearchScreen("'.$error.'"));';
+    }
+    else if ($query) {
+        echo '<script>';
+        echo '$(searchT("'.$query.'"));';
+    }
+    else if ($id) {
+        echo '<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7o8OLl450h1HIJ0b8u-LG8isoMzmXJL4">
+        </script>';
+        echo '<script>';
+        echo '$(showTrainMonitor("'.$id.'"));';
+    }
+    else {
+        echo '<script>';
+        echo '$(showSearchScreen(0));';
+    }
+
+    echo '</script></body></html>';
+
+    ?>
+    
